0e0c89452a7db848e4fc9769e0632113b73f8294
cantosx <cantoscorrea@gmail.com>
2019-01-03 09:58:58 -0200

    Separado em componentes

---
+++ src/components/Bookshelves/Bookshelf.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Book from './Book';
+
+const Bookshelf = (props) => {
+    return (
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">Currently Reading</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            <li>
+              <Book/>
+            </li>            
+          </ol>
+        </div>
+      </div>
+    )
+}
+export default Bookshelf;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import Bookshelves from '../Bookshelves/Bookshelves';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          <Bookshelves />
+        </div>
+        <div className="open-search">
+          <button onClick={() => this.setState({ showSearchPage: true })}>Add a book</button>
+        </div>
+      </div>
+    )
+  }
+}
